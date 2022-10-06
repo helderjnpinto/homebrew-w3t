46be5c2d0153b0f492d3c7a5d8eb9baae88111a4
hp <helder@yarilabs.com>
2022-10-06 14:53:53 +0100

    fix hash

--- Formula/w3t.rb
+++ Formula/w3t.rb
@@ -2,7 +2,7 @@ class W3t < Formula
     desc "web3 terminal utils"
     homepage "https://github.com/helderjnpinto/w3t"
     url "https://github.com/helderjnpinto/w3t/releases/download/v0.0.1/w3t-linux.tar.gz"
-    sha256 "5861feeca2499f3b34b57500f1a39754bf5419900012b7195e3ebe7c11980249"
+    sha256 "e2019854982d81d0beae24e4b9aeecaba148969ee33c79d8aa0794331c8749ea"
     version "0.0.1"
     def install
       bin.install "w3t"
